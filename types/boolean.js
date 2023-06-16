30af9919b4f7dc5ee09fb7abb8710e3557f55842
Nitesh Yadav <ny040021@gmail.com>
2023-06-16 15:28:15 +0530

    Fix boolean field

--- types/boolean.js
+++ types/boolean.js
@@ -1,14 +1,27 @@
 import { DATA_TYPE, DATA_TYPE_TO_COMMON_NAME } from "../constants.js";
-import { ValidatorField, validators } from "../utils.js";
+import { ValidatorField, validators, isNullish } from "../utils.js";
 
 export class BooleanField extends ValidatorField {
+    static defaultIsRequired = false;
+
     constructor(valueIdentifier) {
-        super({valueIdentifier, type: DATA_TYPE.null});
+        super({valueIdentifier, type: DATA_TYPE.null, isRequired: BooleanField.defaultIsRequired});
         this.setErrorMessageInvalidType();
+        this.setErrorMessageRequiredValue();
+    }
+
+    setErrorMessageRequiredValue(message) {
+        this.errorMessageRequiredValue = message !== undefined ? message : `${this.valueIdentifier} is required`;
     }
 
     setErrorMessageInvalidType(message) {
         this.errorMessageInvalidType = message !== undefined ? message : `${this.valueIdentifier} must be ${DATA_TYPE_TO_COMMON_NAME[DATA_TYPE.boolean]}`;
+    }
+
+    required(message) {
+        this.isRequired = true;
+        this.setErrorMessageRequiredValue(message);
+        return this;
     }
 
     invalidTypeMessage(message = undefined) {
@@ -17,6 +30,13 @@ export class BooleanField extends ValidatorField {
     }
 
     validate(value) {
+        if(isNullish(value)){
+            if(this.isRequired) {
+                return this.errorMessageRequiredValue;
+            }
+            return null;
+        }
+
         if(!validators[DATA_TYPE.null](value)) {
             return this.errorMessageInvalidType;
         }
